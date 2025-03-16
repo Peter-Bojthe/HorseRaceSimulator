ccf2092aa26f7aa78e551e364b94375895efdc8f
Peter Bojthe <peterb060208@gmail.com>
2025-03-16 18:55:41 +0000

    Horse Class Changes

--- Part 1/Horse.java
+++ Part 1/Horse.java
@@ -1,76 +1,88 @@
-
 /**
  * Write a description of class Horse here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Every horse must have a name and a character.
+ * They must also have a conficence rating 0-1.
+ * Higher the confidence rating the faster the horse, but more likely to fall
+ * They need to have a boolean flag to check if they have fell
+ * Horse distance in whole numbers
+ * Winning increases confidence, falling decreases it.
+ * 
+ * @author Peter Bojthe 
+ * @version 16/03/25
  */
-public class Horse
-{
+
+public class Horse {
     //Fields of class Horse
+    char horseSymbol;
+    String horseName;
+    double horseConfidence;
+    int horseDistance;
+    boolean horseFallen;
     
-    
-      
     //Constructor of class Horse
     /**
      * Constructor for objects of class Horse
+     * Give Symbol
+     * Give Name
+     * Give Confidence
+     * Set Distance and Fallen to deafult
      */
-    public Horse(char horseSymbol, String horseName, double horseConfidence)
-    {
-       
+    public Horse(char horseSymbol, String horseName, double horseConfidence) {
+       this.horseSymbol = horseSymbol;
+       this.horseName = horseName;
+       this.horseConfidence = horseConfidence;
+       this.horseDistance = 0;
+       this.horseFallen = false;
     }
     
-    
-    
-    //Other methods of class Horse
-    public void fall()
-    {
-        
+    //Horse has fell
+    public void fall() {
+        this.horseFallen = true;
     }
     
-    public double getConfidence()
-    {
-        
+    // Get the confidence of horse
+    public double getConfidence() {
+        return this.horseConfidence;
     }
     
-    public int getDistanceTravelled()
-    {
-        
+    // Get the distance of horse
+    public int getDistanceTravelled() {
+        return this.horseDistance;
     }
     
-    public String getName()
-    {
-        
+    // Get the name of horse
+    public String getName() {
+        return this.horseName;
     }
     
-    public char getSymbol()
-    {
-        
+    // Get the symbol of horse
+    public char getSymbol() {
+        return this.horseSymbol;
     }
     
-    public void goBackToStart()
-    {
-        
+    // Set the position to default
+    public void goBackToStart() {
+        this.horseDistance = 0;
     }
     
-    public boolean hasFallen()
-    {
-        
-    }
-
-    public void moveForward()
-    {
-        
+    // Set the horse to fallen
+    public boolean hasFallen() {
+        return this.horseFallen;
     }
 
-    public void setConfidence(double newConfidence)
-    {
-        
+    // Set position of horse + 1
+    public void moveForward() {
+        this.horseDistance = this.horseDistance+1;
+    }
+
+    // Set confidence of horse to new value 0-1
+    public void setConfidence(double newConfidence) {
+        this.horseConfidence = newConfidence;
     }
     
-    public void setSymbol(char newSymbol)
-    {
-        
+    // Set symbol of the horse to new character
+    public void setSymbol(char newSymbol) {
+        this.horseSymbol = newSymbol;
     }
-    
 }
